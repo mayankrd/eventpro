25a852032cbc2d2fbbe79622cd5def96921f330d
rbadami <badami.r@husky.neu.edu>
2017-12-11 15:43:29 -0500

    Added changes to event schema and user schema

--- server-side/model/events/EventSchema.js
+++ server-side/model/events/EventSchema.js
@@ -11,7 +11,7 @@ module.exports = function () {
     contact: [PhoneSchema],
     Category : CategorySchema,
     users :[String],
-    organiser:[String],
+    organiser:String,
     comments :[CommentSchema]
   }, {collection: 'events'});
 
