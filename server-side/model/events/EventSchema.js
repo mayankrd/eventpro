49c48aad15d398b9a0aca0e63ee6f3a50ca6d93c
rbadami <badami.r@husky.neu.edu>
2017-12-11 13:13:55 -0500

    Added category to Event Schema

--- server-side/model/events/EventSchema.js
+++ server-side/model/events/EventSchema.js
@@ -2,11 +2,13 @@ module.exports = function () {
   var mongoose = require('mongoose');
   var AddressSchema = require('../Address/AddressSchema.js')();
   var PhoneSchema = require('../Phone/PhoneSchema.js')();
+  var CategorySchema = require('../category/Category.schema.server')();
   var EventSchema = mongoose.Schema({
     eventname: String,
     eventdescription: String,
     location: AddressSchema,
     contact: [PhoneSchema],
+    Category : CategorySchema,
     users :[String]
   }, {collection: 'events'});
 
