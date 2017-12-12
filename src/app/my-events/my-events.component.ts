232e1bc3448b68b634ab3f4f238bdc95ef7e0d8a
mayankrd <mayank.dyl@gmail.com>
2017-12-12 12:31:37 -0500

    unregister added

--- src/app/my-events/my-events.component.ts
+++ src/app/my-events/my-events.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {SharedDataService} from "../services/shared.data.service";
+import {UserService} from "../services/user.client.service";
 
 @Component({
   selector: 'app-my-events',
@@ -9,18 +10,53 @@ import {SharedDataService} from "../services/shared.data.service";
 export class MyEventsComponent implements OnInit {
 
   events: any;
+  successFlag: boolean;
 
-  constructor(private sharedDataService: SharedDataService) { }
+  constructor(private sharedDataService: SharedDataService, private userService: UserService) { }
 
   ngOnInit() {
 
-    this.events = this.sharedDataService.user.events;
-    console.log(this.events, this.sharedDataService.user.events);
-    console.log(this.events, this.sharedDataService.user.events);
+    this.events = this.sharedDataService.user.events; //
+
 
   }
 
   unRegister(event){
+
+    var id = event._id;
+
+    var events = this.sharedDataService.user.events;
+    var newEvents = [];
+
+    for(var i=0; i<events.length; i++){
+      console.log(events[0]._id);
+      if(events[i]._id == id){
+        console.log(events[i]._id);
+      }
+      else{
+        newEvents.push(events[i]);
+      }
+    }
+
+    this.sharedDataService.user.events = newEvents;
+
+    this.userService.updateUser(this.sharedDataService.user)
+      .subscribe(
+        (data) => {
+          console.log(data);
+          this.userService.findUserById(this.sharedDataService.user._id)
+            .subscribe(
+              (data) => {
+                console.log('update user', data);
+                this.sharedDataService.user = data;
+                this.successFlag = true;
+                this.events = this.sharedDataService.user.events;
+              }
+            );
+
+          this.sharedDataService.user = data;
+        }
+      );
 
   }
 
